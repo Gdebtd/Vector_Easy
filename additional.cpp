85bd6eeb73841206850292e1ec0abf2746935f95
Gdebtd <76738374+Gdebtd@users.noreply.github.com>
2021-12-15 23:19:43 +0300

    additional

--- additional.cpp
+++ additional.cpp
@@ -1,11 +1,5 @@
 #include "easy_list.h"
 
-void print_vector(vector <int> & mass)
-{
-	for (unsigned int i = 0; i < mass.size(); i++)
-		cout << mass[i] << " ";
-}
-
 void get_swap(int & a, int & b)
 {
     int tmp = a;
