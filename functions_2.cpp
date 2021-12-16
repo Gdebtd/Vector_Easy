762a5949cb14c9d032fb58dcc7f5e75bba5ae898
Gdebtd <76738374+Gdebtd@users.noreply.github.com>
2021-12-16 20:32:33 +0300

    functions_2

--- functions_2.cpp
+++ functions_2.cpp
@@ -2,36 +2,56 @@
 
 void itc_rev_list(vector <int> & mass)
 {
-    int maxi = mass.size();
-    vector <int> res(maxi);
-    for (int i = 0; i < maxi; i++)
+    if (mass.size() != 0)
     {
-        res[i] = mass[maxi - 1 - i];
+        int maxi = mass.size();
+        vector <int> res(maxi);
+        for (int i = 0; i < maxi; i++)
+        {
+            res[i] = mass[maxi - 1 - i];
+        }
+        mass = res;
     }
-    mass = res;
 }
 
 void itc_rev_par_list(vector <int> & mass)
 {
-    for (int i = 0; i < mass.size() - 1; i += 2)
+    if (mass.size() != 0)
     {
-        get_swap(mass[i], mass[i + 1]);
+        for (int i = 0; i < mass.size() - 1; i += 2)
+        {
+            get_swap(mass[i], mass[i + 1]);
+        }
     }
 }
 
 void itc_rshift_list(vector <int> & mass)
 {
-    for (int i = mass.size() - 1; i > 0; i--)
+    int maxi = mass.size();
+    if (maxi != 0)
     {
-        get_swap(mass[i], mass[i - 1]);
+        vector <int> res(maxi);
+        res[0] = mass[maxi - 1];
+        for (int i = 1; i < maxi; i++)
+        {
+            res[i] = mass[i - 1];
+        }
+        mass = res;
     }
 }
 
 void itc_lshift_list(vector <int> & mass)
 {
-    for (int i = 0; i < mass.size() - 1; i++)
+    int maxi = mass.size();
+    if (maxi != 0)
     {
-        get_swap(mass[i], mass[i + 1]);
+        vector <int> res(maxi);
+        res[maxi - 1] = mass[0];
+        for (int i = 0; i < maxi - 1; i++)
+        {
+            res[i] = mass[i + 1];
+        }
+        mass = res;
     }
 }
 
