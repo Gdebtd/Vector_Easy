4e81a1b7f11f17697900d03c5e22a5bfcb90afa5
Gdebtd <76738374+Gdebtd@users.noreply.github.com>
2021-12-16 20:34:48 +0300

    functions_1

--- functions_1.cpp
+++ functions_1.cpp
@@ -11,7 +11,7 @@ void itc_even_index_list (const vector <int> & mass, vector <int> & mass2)
 
 void itc_even_parts_list (const vector <int> & mass, vector <int> & mass2)
 {
-    for (int i = 0; i < mass.size(); i++)
+	for (int i = 0; i < mass.size(); i++)
 	{
 		if (i % 2 != 0)
 			mass2.push_back(mass[i]);
@@ -32,17 +32,20 @@ int itc_positive_list(const vector <int> & mass)
 int itc_sl_list(const vector <int> & mass)
 {
     int cnt = 0;
-    for (int i = 0; i < mass.size() - 1; i++)
+    if (mass.size() != 0)
     {
-        if (mass[i + 1] > mass[i])
-            cnt++;
+	    for (int i = 0; i < mass.size() - 1; i++)
+	    {
+		if (mass[i + 1] > mass[i])
+		    cnt++;
+	    }
     }
     return cnt;
 }
 
 bool itc_same_parts_list(const vector <int> & mass)
 {
-    if (mass.size() > 1)
+    if (mass.size() != 0)
     {
         for (int i = 0; i < mass.size() - 1; i++)
         {
